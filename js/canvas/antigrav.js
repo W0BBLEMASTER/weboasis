344b4467be660baefd0a034e6978ecde45c9243d
Ẅ̶̧́̏͐Ö̴͙̠͙̥̖́̓̾̿͝B̴͇̩̆B̷̫̫͕̔̀̇̕͠L̸̜̈E̴̬͈͘̕͜M̶̢̹̹̝̥̄̊Ȃ̴̢͝S̵͍̙͗̓̋̚͜T̸̨͈̠̦͌͊̈́̚͝E̴͎̩̥̫͗̋R̵̪̞̭̘̘̀ ̷̻̹͇̟̓́̒ <60029414+W0BBLEMASTER@users.noreply.github.com>
2025-11-27 18:42:37 -0500

    Update antigrav.js

--- js/canvas/antigrav.js
+++ js/canvas/antigrav.js
@@ -7,7 +7,7 @@ t.style.backgroundColor = "black";
 // Create Video Element for the Gradient
 var v = document.createElement("video");
 // Pointing to the asset on GitHub (assuming Antigravity_Rip folder is uploaded to root)
-v.src = "https://raw.githack.com/W0BBLEMASTER/weboasis/master/js/canvas/Dash-Gradient.mp4";
+v.src = "https://cdn.jsdelivr.net/gh/W0BBLEMASTER/weboasis@master/js/canvas/Dash-Gradient.mp4";
 v.autoplay = true;
 v.loop = true;
 v.muted = true;
@@ -19,3 +19,7 @@ v.style.position = "absolute";
 v.style.top = "0";
 v.style.left = "0";
 v.style.zIndex = "0"; // Behind content
+
+
+
+t.appendChild(v);
