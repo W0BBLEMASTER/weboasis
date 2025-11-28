ca115c52eaecb3a9e69a4460d21074affcd31196
Ẅ̶̧́̏͐Ö̴͙̠͙̥̖́̓̾̿͝B̴͇̩̆B̷̫̫͕̔̀̇̕͠L̸̜̈E̴̬͈͘̕͜M̶̢̹̹̝̥̄̊Ȃ̴̢͝S̵͍̙͗̓̋̚͜T̸̨͈̠̦͌͊̈́̚͝E̴͎̩̥̫͗̋R̵̪̞̭̘̘̀ ̷̻̹͇̟̓́̒ <60029414+W0BBLEMASTER@users.noreply.github.com>
2025-11-27 19:28:19 -0500

    Readded fixed antigrav.js final.

---
+++ js/canvas/antigrav.js
@@ -0,0 +1,23 @@
+var t = document.getElementById("canvasContainer");
+while (t.firstChild) {
+    t.removeChild(t.firstChild);
+}
+t.style.backgroundColor = "black";
+
+// Create Video Element for the Gradient
+var v = document.createElement("video");
+// Pointing to the asset on GitHub (assuming Antigravity_Rip folder is uploaded to root)
+v.src = "https://raw.githack.com/W0BBLEMASTER/weboasis/master/js/canvas/Dash-Gradient.mp4";
+v.autoplay = true;
+v.loop = true;
+v.muted = true;
+v.playsInline = true;
+v.style.width = "100%";
+v.style.height = "100%";
+v.style.objectFit = "cover";
+v.style.position = "absolute";
+v.style.top = "0";
+v.style.left = "0";
+v.style.zIndex = "0"; // Behind content
+
+t.appendChild(v);
